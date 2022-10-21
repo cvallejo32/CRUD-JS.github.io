73f17a5ec7ced1902728d729eb108e895986176c
cvallejo32 <crvallejo.dwfc6@ucamp.utel.edu.mx>
2022-10-21 17:00:13 -0500

    Cambios nuevos JS logica

--- javascript/index.js
+++ javascript/index.js
@@ -2,30 +2,43 @@ users = [
     {
         user: 'User 1',
         workout: 'Press'
-    }
+    },
     {
         user: 'User 2',
         workout: 'Joggin'
-    }
+    },
     {
         user: 'User 3',
         workout: 'Swiming'
-    }
+    },
 ]
 
 let showUsers = (receivedUsers) => {
-    document.getElementsByTagName("tbody"){0}.innerHTML = '';
+    document.getElementsByTagName("tbody")[0].innerHTML = '';
     receivedUsers.forEach((element, index) => {
         let texth1 = document.createElement("tr");
         texth1.innerHTML = `
-        <td scope="row">${index + 1}</td>   
+        <td scope="row">${index+1}</td>   
         <td>${element.user}</td>
         <td>${element.workout}</td>
         <td>
-        <button type="button" class="btn btn-primary" onclick="editRegistry()">Edit</button>
-        <button type="button" class="btn btn-danger" onclick="deleteRegistry()">Delete</button>
+        <button type="button" class="btn btn-primary" onclick="editRegistry()">
+        Edit
+        </button>
+        <button type="button" class="btn btn-danger" onclick="deleteRegistry()">
+        Delete
+        </button>
         </td>`;
         document.getElementsByTagName("tbody")[0].appendChild(texth1);
     });
 };
-{}
+
+let deleteRegistry = () =>{
+    console.log("Eliminar")
+};
+
+let editRegistry = () =>{
+    console.log("Editar")
+}
+
+showUsers();
