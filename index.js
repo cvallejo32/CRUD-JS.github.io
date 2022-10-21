20bcecfc50e0dde63c9f86c04be5af3b79bd686f
cvallejo32 <crvallejo.dwfc6@ucamp.utel.edu.mx>
2022-10-20 20:27:58 -0500

    Estructura basica, bootstrap y js

---
+++ index.js
@@ -0,0 +1,14 @@
+users = [
+    {
+        name: 'User 1',
+        workout: 'Press'
+    }
+    {
+        name: 'User 2',
+        workout: 'Joggin'
+    }
+    {
+        name: 'User 3',
+        workout: 'Swiming'
+    }
+]
