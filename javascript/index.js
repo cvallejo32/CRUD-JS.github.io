c07a6f320a6316fb7c0e0a4c0b3ea43b82c75e3f
cvallejo32 <crvallejo.dwfc6@ucamp.utel.edu.mx>
2022-10-21 18:28:50 -0500

    Logica de borrar, editar y actualizar

--- javascript/index.js
+++ javascript/index.js
@@ -13,19 +13,22 @@ users = [
     },
 ]
 
+
+
+
 let showUsers = (receivedUsers) => {
     document.getElementsByTagName("tbody")[0].innerHTML = '';
     receivedUsers.forEach((element, index) => {
         let texth1 = document.createElement("tr");
         texth1.innerHTML = `
-        <td scope="row">${index+1}</td>   
+        <td scope="row">${index + 1}</td>   
         <td>${element.user}</td>
         <td>${element.workout}</td>
         <td>
-        <button type="button" class="btn btn-primary" onclick="editRegistry()">
+        <button type="button" class="btn btn-primary" onclick="editRegistry(${index})">
         Edit
         </button>
-        <button type="button" class="btn btn-danger" onclick="deleteRegistry()">
+        <button type="button" class="btn btn-danger" onclick="deleteRegistry(${index})">
         Delete
         </button>
         </td>`;
@@ -33,12 +36,26 @@ let showUsers = (receivedUsers) => {
     });
 };
 
-let deleteRegistry = () =>{
-    console.log("Eliminar")
+let deleteRegistry = (parIndex) =>{
+    users = users.filter((user, index) => index !== parIndex);
+    showUsers(users);
 };
 
-let editRegistry = () =>{
-    console.log("Editar")
-}
+let editRegistry = (parIndex) =>{
+    document.getElementById('position').value = parIndex;
+    document.getElementById('nameUser').value = users[parIndex].user;
+    document.getElementById('workout').value = users[parIndex].workout;
+};
+
+let updateRegistry = () => {
+    let position = document.getElementById('position').value;
+    let nameUser = document.getElementById('nameUser').value;
+    let workout = document.getElementById('workout').value;
+
+    if (position == '' || isNaN(position)) {
+        alert('You must select a registry!');
+        return false;
+    }
+};
 
 showUsers();
