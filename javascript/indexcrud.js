24af35af95e6d109f743291025468c20969c7c2d
cvallejo32 <crvallejo.dwfc6@ucamp.utel.edu.mx>
2022-10-21 22:51:07 -0500

    Se configuro todo el local storage

--- javascript/indexcrud.js
+++ javascript/indexcrud.js
@@ -23,3 +23,58 @@ let showUsers = () => {
         document.getElementsByTagName("tbody")[0].appendChild(texth1);
     });
 }
+
+let deleteRegistry = (parIndex) =>{
+    storageUsers = JSON.parse(localStorage.getItem('usersls'))
+    storageUsers = storageUsers.filter((user, index) => index !== parIndex)
+    localStorage.setItem('usersls', JSON.stringify(storageUsers));
+    showUsers();
+};
+
+let fillForm = (parIndex) =>{
+    storageUsers = JSON.parse(localStorage.getItem('usersls'))
+    document.getElementById('position').value = parIndex;
+    document.getElementById('nameUser').value = storageUsers[parIndex].user;
+    document.getElementById('workout').value = storageUsers[parIndex].workout;
+};
+
+let updateRegistry = () => {
+    let position = document.getElementById('position').value;
+    let nameUser = document.getElementById('nameUser').value;
+    let workout = document.getElementById('workout').value;
+    if (position == '' || isNaN(position)) {
+        alert('You must select a registry!');
+        return false;
+    }
+    storageUsers = JSON.parse(localStorage.getItem('usersls'))
+    storageUsers[position] = {
+        user: nameUser,
+        workout: workout
+    }
+    document.getElementById('position').value = '';
+    document.getElementById('nameUser').value = '';
+    document.getElementById('workout').value = '';
+    localStorage.setItem('usersls', JSON.stringify(storageUsers));
+    showUsers
+};
+
+let addRegistry = () => {
+    usersLS = JSON.parse(localStorage.getItem('usersls'))
+    const newUser = {
+        user: document.getElementById('nameUser').value,
+        workout: document.getElementById('workout').value
+    }
+    document.getElementById('nameUser').value = '';
+    document.getElementById('workout').value = '';
+    usersLS.push(newUser);
+    localStorage.setItem('usersls', JSON.stringify(usersLS));
+    showUsers();
+}
+
+let clearCache = () => {
+    localStorage.clear();
+    alert('Local storage memory has been erased');
+    showUsers();
+}
+
+showUsers();
