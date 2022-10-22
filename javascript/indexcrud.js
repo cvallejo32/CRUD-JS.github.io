817acb2a8463c874d67659ce05f8aeb2ca51d305
cvallejo32 <crvallejo.dwfc6@ucamp.utel.edu.mx>
2022-10-22 09:49:58 -0500

    crud finished

--- javascript/indexcrud.js
+++ javascript/indexcrud.js
@@ -2,78 +2,78 @@ let showUsers = () => {
     let usersLS = JSON.parse(localStorage.getItem('usersls'))
     if( usersLS === null){
         console.log('entro');
-        localStorage.setItem('usersls', JSON.stringify([]));
+        localStorage.setItem('usersls',  JSON.stringify([]));
         usersLS = []
     }
     document.getElementsByTagName("tbody")[0].innerHTML = '';
     usersLS.forEach((element, index) => {
         let texth1 = document.createElement("tr");
         texth1.innerHTML = `
-        <td scope="row">${index + 1}</td>   
-        <td>${element.user}</td>
+        <td scope="row">${index + 1}</td>
+        <td>${element.name}</td>
         <td>${element.workout}</td>
         <td>
-        <button type="button" class="btn btn-primary" onclick="editRegistry(${index})">
-        Edit
-        </button>
-        <button type="button" class="btn btn-danger" onclick="deleteRegistry(${index})">
-        Delete
-        </button>
+            <button type="button" class="btn btn-primary" onclick="fillForm(${index})">
+                Edit
+            </button>
+            <button type="button" class="btn btn-danger" onclick="deleteRegistry(${index})">
+                Delete
+            </button>
         </td>`;
         document.getElementsByTagName("tbody")[0].appendChild(texth1);
     });
 }
 
-let deleteRegistry = (parIndex) =>{
-    storageUsers = JSON.parse(localStorage.getItem('usersls'))
-    storageUsers = storageUsers.filter((user, index) => index !== parIndex)
-    localStorage.setItem('usersls', JSON.stringify(storageUsers));
+let deleteRegistry = (parIndex) => {
+    savedUsers = JSON.parse(localStorage.getItem('usersls'))
+    savedUsers = savedUsers.filter((alumno, index) => index !== parIndex);
+    localStorage.setItem('usersls',  JSON.stringify(savedUsers));
     showUsers();
-};
+}
 
-let fillForm = (parIndex) =>{
-    storageUsers = JSON.parse(localStorage.getItem('usersls'))
+let fillForm = (parIndex) => {
+    savedUsers = JSON.parse(localStorage.getItem('usersls'))
     document.getElementById('position').value = parIndex;
-    document.getElementById('nameUser').value = storageUsers[parIndex].user;
-    document.getElementById('workout').value = storageUsers[parIndex].workout;
-};
+    document.getElementById('nameUser').value = savedUsers[parIndex].name;
+    document.getElementById('workout').value = savedUsers[parIndex].workout;
+}
 
 let updateRegistry = () => {
     let position = document.getElementById('position').value;
     let nameUser = document.getElementById('nameUser').value;
     let workout = document.getElementById('workout').value;
     if (position == '' || isNaN(position)) {
-        alert('You must select a registry!');
+        alert('Debe seleccionar un registro');
         return false;
     }
-    storageUsers = JSON.parse(localStorage.getItem('usersls'))
-    storageUsers[position] = {
-        user: nameUser,
+    savedUsers = JSON.parse(localStorage.getItem('usersls'))
+    savedUsers[position] = {
+        name: nameUser,
         workout: workout
     }
     document.getElementById('position').value = '';
     document.getElementById('nameUser').value = '';
     document.getElementById('workout').value = '';
-    localStorage.setItem('usersls', JSON.stringify(storageUsers));
-    showUsers
-};
+    localStorage.setItem('usersls',  JSON.stringify(savedUsers));
+    showUsers();
+}
 
 let addRegistry = () => {
     usersLS = JSON.parse(localStorage.getItem('usersls'))
     const newUser = {
-        user: document.getElementById('nameUser').value,
+        name: document.getElementById('nameUser').value,
         workout: document.getElementById('workout').value
     }
     document.getElementById('nameUser').value = '';
     document.getElementById('workout').value = '';
     usersLS.push(newUser);
-    localStorage.setItem('usersls', JSON.stringify(usersLS));
+    localStorage.setItem('usersls',  JSON.stringify(usersLS));
     showUsers();
 }
 
 let clearCache = () => {
-    localStorage.clear();
-    alert('Local storage memory has been erased');
+    localStorage.clear();    
+    alert('Localstorage memory was erased');
     showUsers();
 }
 
